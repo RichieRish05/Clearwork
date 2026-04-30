973341e27b1ba721152f946aa18410ac3cd58a29
Rishi Murumkar <108751289+RichieRish05@users.noreply.github.com>
2026-04-29 20:49:18 -0700

    added nav bar

--- site/app/(main)/(home)/page.tsx
+++ site/app/(main)/(home)/page.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 
 import { GridPattern } from "@/components/ui/grid-pattern";
+import { Navbar1 } from "@/components/ui/navbar-1";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col flex-1 items-center justify-center bg-white font-sans">
+    <div className="relative flex flex-col flex-1 items-center bg-white font-sans">
       <GridPattern
         width={56}
         height={56}
-        className="mask-[radial-gradient(ellipse_at_center,black_40%,transparent_85%)]"
+        className="mask-[radial-gradient(ellipse_at_center,black_60%,transparent_95%)]"
       />
+      <Navbar1 />
       <main className="relative flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
 
       </main>
